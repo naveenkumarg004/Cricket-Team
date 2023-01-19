a0b6e7dc65aababe43fd0d0ba1c984d9ce1cbb3a
naveenkumarg004 <122471388+naveenkumarg004@users.noreply.github.com>
2023-01-19 12:25:36 +0530

    Update app.js in github

--- app.js
+++ app.js
@@ -25,18 +25,30 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
-//Get Players API
+//Get Players API 1
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
 
 app.get("/players/", async (request, response) => {
-  const getPlayersQuery = `
-    select *
-    from cricket_team
-    order by player_id `;
-  const playersArray = await db.all(getPlayersQuery);
-  response.send(playersArray);
+const getPlayersQuery = `
+ SELECT
+ *
+ FROM
+ cricket_team;`;
+const playersArray = await database.all(getPlayersQuery);
+ response.send(
+ playersArray.map((eachPlayer) =>
+convertDbObjectToResponseObject(eachPlayer))
+);
 });
 
-//Add Player API
+//Add Player API 2
 app.post("/player/", async (request, response) => {
   const playerDetails = request.body;
   const { playerName, jerseyNumber, role } = playerDetails;
@@ -45,10 +57,50 @@ app.post("/player/", async (request, response) => {
   insert into
   cricket_team (player_name, jersey_number, role)
   values
-  ( ${playerName} ,
+  ( `${playerName}` ,
     ${jerseyNumber} ,
-    ${role} ,
+    `${role}` ,
   ) `;
   const dbResponse = await db.run(addPlayerQuery);
   response.send("Player Added to Team");
 });
+
+//GET PlayerBYId API 3
+
+app.get("/players/:playerId/", (request, response)=>{
+  const {playerId} = request.params
+  const getPlayerQuery = `
+  select *
+  from cricket_team 
+  where played_id = ${playerId}`
+  const playerDetail = await db.get(getPlayerQuery)
+  response.send(convertDbObjectToResponseObject(playerDetail))
+  
+  )
+
+//UPDATE PUT playerDetails API 4
+  app.put("/players/:playerId/" , async (request, response) =>{
+    const {playerId} = request.params
+    const playerDetail = request.body
+    const {"playerName , jerseyNumber , role} = playerDetail
+    const updatePlayerQuery = `
+    update cricket_team 
+    set player_name = `${playerName}`,
+        jersey_number = ${jerseyNumber} ,
+        role = ${role} 
+     where player_id = ${playerId}` 
+    await db.run(updatePlayerQuery)
+    response.send("Player Details Added"
+    })
+
+//DELETE API 5
+app.delete("/players/:playerId/" , async(request, response) =>{
+  const {playerId} = request.params
+  const delPlayerQuery = `
+  delete from
+  cricket_team
+  where player_id = ${playerId}`
+  await db.run(delPlayerQuery)
+  response.send("Player Removed")
+})
+module.exports = app ;
